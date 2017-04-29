a71afff33ffe3eed5f433c90f3977d76c4b1f9a8
akitsu-sanae <akitsu.sanae@gmail.com>
2017-04-29 14:37:17 +0900

    nyna

--- constanze/union-find.hpp
+++ constanze/union-find.hpp
@@ -23,65 +23,72 @@ struct union_find {
     using value_t = T;
 
     std::size_t depth(element_t e) const {
-        std::size_t result = 0;
-        while (true) {
-            auto found = edges.find(e);
-            if (found == edges.end())
-                throw std::logic_error{"element not found"};
-            if (found->second == nullptr)
-                break;
-            e = *found->second;
-            ++result;
-        }
+        auto found = data.find(e);
+        if (found == data.end())
+            throw std::logic_error{"element not found"};
+        if (found->second == nullptr)
+            return 0;
+        else
+            return 1 + depth(*found->second);
+    }
+    element_t root(element_t e) {
+        auto found = data.find(e);
+        if (found == data.end())
+            throw std::logic_error{"element not found"};
+        if (found->second == nullptr)
+            return e;
+        auto result = root(*found->second);
+        found->second = std::make_shared<T>(result);
         return result;
     }
+
     element_t root(element_t e) const {
-        while (true) {
-            auto found = edges.find(e);
-            if (found == edges.end())
-                throw std::logic_error{"element not found"};
-            if (found->second == nullptr)
-                break;
-            e = *found->second;
-        }
-        return e;
+        auto found = data.find(e);
+        if (found == data.end())
+            throw std::logic_error{"element not found"};
+        if (found->second == nullptr)
+            return e;
+        return root(*found->second);
     }
 
     self& add(element_t const& v) {
-        edges[v] = nullptr;
+        data[v] = nullptr;
         return *this;
     }
     self& add(std::initializer_list<element_t> const& list) {
         for (auto const& v : list) {
             if (&v == list.begin())
-                edges[v] = nullptr;
+                data[v] = nullptr;
             else
-                edges[v] = std::make_shared<element_t>(*list.begin());
+                data[v] = std::make_shared<element_t>(*list.begin());
         }
         return *this;
+    }
+    bool exist(element_t const& e) const {
+        return data.find(e) != data.end();
     }
 
     self& unite(element_t const& lhs, element_t const& rhs) {
         auto lhs_root = root(lhs);
         auto rhs_root =  root(rhs);
         if (depth(lhs) < depth(rhs))
-            edges[lhs_root] = std::make_shared<element_t>(rhs_root);
+            data[lhs_root] = std::make_shared<element_t>(rhs_root);
         else
-            edges[rhs_root] = std::make_shared<element_t>(lhs_root);
+            data[rhs_root] = std::make_shared<element_t>(lhs_root);
         return *this;
     }
-    bool find(element_t const& lhs, element_t const& rhs) const {
+    bool find(element_t const& lhs, element_t const& rhs) {
         return root(lhs) == root(rhs);
     }
 
-    std::map<element_t, std::shared_ptr<element_t>> edges; // element to parent
+    std::map<element_t, std::shared_ptr<element_t>> data; // element to parent
 };
 
 template<typename T> inline static std::ostream&
 operator<<(std::ostream& os, union_find<T> const& uf) {
     //     root     nodes
     std::map<T, std::vector<T>> groups;
-    for (auto const& e : uf.edges) {
+    for (auto const& e : uf.data) {
         auto root = uf.root(e.first);
         groups[root].push_back(e.first);
     }
